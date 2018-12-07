3d1a391a5154469a79fb79c5d8677c688c70aa1a
Teng <Natasha.Teng@investorsgroup.com>
2018-12-07 13:33:33 -0500

    Created weighted list

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,41 @@
-const pool = {
-    0: 'r-level',
-    1: 'sr-level',
-    2: 'ssr-level'
-}
+//lists
+const pool = [
+    'R', 'SR', 'SSR'
+]
+//probability
+const weight = [
+    0.82, 0.15, 0.03
+]
 //Weighted Random Number Generation
 //1 roll R 82%; SR 15%; SSR 3%;
 // In a 10 roll, the last item is 97%SR/3%SSR
+const weightedList = function(pool, weight){
+    const weighed_list = [];
+    
+    //rewrite as forEach
 
+    // Loop over weights
+    for (var i = 0; i < weight.length; i++) {
+        var multiples = weight[i] * 100;
+         
+        // Loop over the list of items
+        for (var j = 0; j < multiples; j++) {
+            weighed_list.push(pool[i]);
+        }
+    }
+     
+    return weighed_list;
+}
+
+var weighed_list = weightedList(pool, weight);
+console.log(weighed_list);
+
+
+//Puls random card from the weighted list
 function randoCard (){
-    const keys = Object.keys(pool).length
-    const randoInt = Math.floor(Math.random() * keys)
-    const getCard = pool[randoInt];
-    console.log(getCard);
+    const randoInt = Math.floor(Math.random() * weighed_list.length)
+    const getCard = weighed_list[randoInt];
+    console.log(randoInt, getCard);
 }
 
 randoCard();
