3560adb11867f75214962b0bfdf7aac2360bbc69
Teng <Natasha.Teng@investorsgroup.com>
2019-01-03 09:27:57 -0500

    reformat and add single roll

--- scripts/script.js
+++ scripts/script.js
@@ -10,48 +10,72 @@ const pool = [
 const weight = [
     0.82, 0.15, 0.03
 ]
-const rateUp = [
+const rateUpWeight = [
     0, 0.97, 0.03
 ]
 //Weighted Random Number Generation
 //1 roll R 82%; SR 15%; SSR 3%;
-// In a 10 roll, the last item is 97%SR/3%SSR
 const weightedList = function(pool, weight){
-    const weighed_list = [];
+    const weighedList = [];
     // Loop over weights
     for (let i = 0; i < weight.length; i++) {
         const multiples = weight[i] * 100;
+        
+        // Loop over the list of items
+        for (let j = 0; j < multiples; j++) {
+            weighedList.push(pool[i]);
+        }
+    }
+    return weighedList;
+}
+
+// In a 10 roll, the last item is 97%SR/3%SSR
+//Rate ups on 10 roll, last occurence is guaranteed SR and above
+const rateUp = function(pool, rateUpWeight){
+    const weighedList = [];
+    // Loop over weights
+    for (let i = 0; i < rateUpWeight.length; i++) {
+        const multiples = rateUpWeight[i] * 100;
          
         // Loop over the list of items
         for (let j = 0; j < multiples; j++) {
-            weighed_list.push(pool[i]);
+            weighedList.push(pool[i]);
         }
-    }
-     
-    return weighed_list;
+    } 
+    return weighedList;
 }
 
-const weighed_list = weightedList(pool, weight);
+const weighedList = weightedList(pool, weight);
+const rateUpList =rateUp(pool,rateUpWeight);
 
-//Puls random card from the weighted list
+//Pulls random card from the weighted list
 function randoCard (){
-    const randoInt = Math.floor(Math.random() * weighed_list.length)
-    const getCard = weighed_list[randoInt];
+    const randoInt = Math.floor(Math.random() * weighedList.length)
+    const getCard = weighedList[randoInt];
     console.log(randoInt, getCard);
-    //print weighed_list ten times
+}
 
+function singleRoll(){
+    soloyolo.addEventListener('click', function(){
+        //Runs the function rando card 1 time
+        randoCard();
+    })
 }
 
 function tenRoll(){
     roll10.addEventListener('click', function(){
         //Runs the function get rando card 10 times
-        for (let i = 0; i < 10; i++) randoCard(i);
-        console.log("Is clicked");
+        //the last occurence should have different rates
+        for (let i = 0; i < 9; i++) randoCard(i);
     })
     
 }
+function appLoad(){
+    tenRoll();
+    singleRoll();
+}
 
-tenRoll();
+appLoad();
 
 // console.log(randoCard);
 
